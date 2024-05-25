a353b33366ec45d3f84a532ca0a153c1f0e004a4
AlexanderC <alexanderchenhall@protonmail.com>
2024-05-25 21:16:06 +1000

    Added functionality to take a list of ID's (separated by commas) back, allowing you to do lots of ID's at a time.

--- SPT-Blacklister.py
+++ SPT-Blacklister.py
@@ -1,45 +1,59 @@
 import json
 import re
 
+
 def update_cansellonragfair(file_path, item_id, value):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+    try:
 
-    if item_id in data:
-        data[item_id]["_props"]["CanSellOnRagfair"] = value
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
 
-        with open(file_path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=2, ensure_ascii=False)
+        if item_id in data:
+            data[item_id]["_props"]["CanSellOnRagfair"] = value
 
-        print(f"{data[item_id]['_name']} was changed to {value}")
-    else:
-        print("Invalid item ID. Please enter a valid ID.")
+            with open(file_path, 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=2, ensure_ascii=False)
 
-def main():
-    file_path = 'Aki_Data/Server/Database/templates/items.json'
+            print(f"{data[item_id]['_name']} was changed to {value}")
+        else:
+            print("Invalid item ID: {item_id}. Skipping.")
+    except Exception as e:
+        print(f"Error updating item ID {item_id}: {str(e)}")
+
+
+def process_item_ids(file_path, item_ids, cansellonragfair_value):
+    item_ids = item_ids.split(',')
     valid_id_length = 24
-    cansellonragfair_value = False
 
-    while True:
-        print(f"Current blacklist value: {cansellonragfair_value}")
-
-        item_id = input("Enter an item ID (or 'q' to quit, 'r' to toggle blacklist value true or false): ")
-
-        if item_id.lower() == 'q':
-            break
-
-        if item_id.lower() == 'r':
-            cansellonragfair_value = not cansellonragfair_value
-            print(f"Setting blacklist value to {cansellonragfair_value}")
-            continue
-
+    for item_id in item_ids:
         item_id = re.sub(r'[^a-zA-Z0-9]', '', item_id)
-
         if len(item_id) != valid_id_length:
-            print("Invalid ID. Please enter valid ID's")
+            print(f"Invalid item ID length: {item_id}. Skipping.")
+            continue
 
         update_cansellonragfair(file_path, item_id, cansellonragfair_value)
 
+
+def main():
+    file_path = 'Aki_Data/Server/Database/templates/items.json'
+    cansellonragfair_value = False
+
+    while True:
+        print(f"Current Blacklist value: {cansellonragfair_value}")
+        item_ids = input("Enter item IDs, separated by commas. q to quit, r to toggle between True and False: ")
+
+        if item_ids.lower() == 'q':
+            break
+
+        if item_ids.lower() == 'r':
+            cansellonragfair_value = not cansellonragfair_value
+            print(f"Blacklist value toggle to {cansellonragfair_value}")
+            continue
+
+        else:
+            process_item_ids(file_path, item_ids, cansellonragfair_value)
+
+
+
 if __name__ == '__main__':
     main()
-
